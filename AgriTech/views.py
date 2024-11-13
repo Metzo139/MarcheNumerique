e023087f5bdec2160fa0dbe22fa63f5d168146a8
Metzo139 <160592053+Metzo139@users.noreply.github.com>
2024-11-13 10:11:27 +0000

    Bonjour

--- AgriTech/views.py
+++ AgriTech/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Produit
 # Create your views here.
 def index (request):
      return render(request, 'index.html')
+def produits (request):
+     return render(request, 'product.html')
--- marchenumeriqueagricole/urls.py
+++ marchenumeriqueagricole/urls.py
@@ -21,4 +21,5 @@ from AgriTech import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
+    path('produit/',views.produits),
 ]
